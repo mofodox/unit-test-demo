04dc87eed11df38929d5bdfd0abe043ac9ad4ceb
Khairul Akmal <khairul@ohmyhome.com>
2021-08-25 10:03:50 +0800

    uncomment some code

--- user/user_test.go
+++ user/user_test.go
@@ -1,10 +1,9 @@
 package user
 
 import (
-	"errors"
+	"fmt"
 	"testing"
 
-	"unit-test-demo/match"
 	"unit-test-demo/mocks"
 
 	"github.com/golang/mock/gomock"
@@ -25,7 +24,7 @@ func TestUse(t *testing.T) {
 
 	// Using our own matchers
 	// Expect Do to be called once with 123 and any string as parameters, and return nil from the mocked call.
-	mockDoer.EXPECT().DoSomething(123, match.OfType("string")).Return(nil).Times(1)
+	// mockDoer.EXPECT().DoSomething(123, match.OfType("string")).Return(nil).Times(1)
 
 	// GoMock provides a way to assert that one call must happen after another call, the .After method
 	// callFirst := mockDoer.EXPECT().DoSomething(1, "first this")
@@ -33,12 +32,17 @@ func TestUse(t *testing.T) {
 	// callB := mockDoer.EXPECT().DoSomething(2, "or this").After(callFirst)
 
 	// GoMock also provides a convenience function gomock.InOrder to specify that the calls must be performed in the exact order given. This is less flexible than using .After directly, but can make your tests more readable for longer sequences of calls
-	gomock.InOrder(
-		mockDoer.EXPECT().DoSomething(1, "first this"),
-		mockDoer.EXPECT().DoSomething(2, "then this"),
-		mockDoer.EXPECT().DoSomething(3, "then this"),
-		mockDoer.EXPECT().DoSomething(4, "finally this"),
-	)
+	// gomock.InOrder(
+	// 	mockDoer.EXPECT().DoSomething(1, "first this").Return(nil).Times(1),
+	// 	mockDoer.EXPECT().DoSomething(2, "then this").Return(nil).Times(1),
+	// 	mockDoer.EXPECT().DoSomething(3, "then this").Return(nil).Times(1),
+	// 	mockDoer.EXPECT().DoSomething(4, "finally this").Return(nil).Times(1),
+	// )
+
+	// Specifying mock actions
+	mockDoer.EXPECT().DoSomething(123, gomock.Any()).Return(nil).Do(func(x int, y string) {
+		fmt.Println("Called with x =", x, "and y =", y)
+	})
 
 	err := testUser.Use()
 	if err != nil {
@@ -46,19 +50,19 @@ func TestUse(t *testing.T) {
 	}
 }
 
-func TestUseReturnsErrorFromDo(t *testing.T) {
-	mockController := gomock.NewController(t)
-	defer mockController.Finish()
+// func TestUseReturnsErrorFromDo(t *testing.T) {
+// 	mockController := gomock.NewController(t)
+// 	defer mockController.Finish()
 
-	dummyError := errors.New("dummy error")
-	mockDoer := mocks.NewMockDoer(mockController)
-	testUser := User{Doer: mockDoer}
+// 	dummyError := errors.New("dummy error")
+// 	mockDoer := mocks.NewMockDoer(mockController)
+// 	testUser := User{Doer: mockDoer}
 
-	// Expect Do to be called once with 123 and "Hello GoMock" as parameters, and return dummyError from the mocked call.
-	mockDoer.EXPECT().DoSomething(123, "Hello GoMock").Return(dummyError).Times(1)
+// 	// Expect Do to be called once with 123 and "Hello GoMock" as parameters, and return dummyError from the mocked call.
+// 	mockDoer.EXPECT().DoSomething(123, "Hello GoMock").Return(dummyError).Times(1)
 
-	err := testUser.Use()
-	if err != nil {
-		t.Fail()
-	}
-}
+// 	err := testUser.Use()
+// 	if err != nil {
+// 		t.Fail()
+// 	}
+// }
