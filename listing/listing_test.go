ecc812fe497ef87b9e41638a7d6657644ebad446
Khairul Akmal <khairul@ohmyhome.com>
2021-08-25 15:25:56 +0800

    Update the listing_test.go

--- listing/listing_test.go
+++ listing/listing_test.go
@@ -1,51 +1,55 @@
-package listing
+package listing_test
 
-import "testing"
+import (
+	"testing"
+
+	"unit-test-demo/listing"
+)
 
 func TestCalculateCommission(t *testing.T) {
-	listing := Listing{
+	listingStruct := listing.Listing{
 		Id:    "1",
 		Name:  "11 Jackson Square",
 		Price: 1000000.00,
 	}
 
 	// Find the commission by using the CalculateCommission function
-	commission := CalculateCommission(listing.Price)
+	commission := listing.CalculateCommission(listingStruct.Price)
 	// We know that to find the commission of a listing, the formula is: price * 0.03
 	expectedCommission := 30000.00
 
 	// Assert
 	if commission != expectedCommission {
-		t.Errorf("commission %.2f expected commission %.2f listing price %.2f", commission, expectedCommission, listing.Price)
+		t.Errorf("commission %.2f expected commission %.2f listing price %.2f", commission, expectedCommission, listingStruct.Price)
 	}
 }
 
 func TestCalculatePriceAfterCommission(t *testing.T) {
-	listing := Listing{
+	listingStruct := listing.Listing{
 		Id:    "1",
 		Name:  "11 Jackson Square",
 		Price: 1000000.00,
 	}
 
-	priceAfterCommission := CalculatePriceAfterCommission(listing.Price)
+	priceAfterCommission := listing.CalculatePriceAfterCommission(listingStruct.Price)
 	expectedPriceAfterCommission := 970000.00
 
 	if priceAfterCommission != expectedPriceAfterCommission {
-		t.Errorf("price after commission %.2f expected price after commission %.2f listing price %.2f", priceAfterCommission, expectedPriceAfterCommission, listing.Price)
+		t.Errorf("price after commission %.2f expected price after commission %.2f listing price %.2f", priceAfterCommission, expectedPriceAfterCommission, listingStruct.Price)
 	}
 }
 
 /********* Table Driven Tests *********/
 
 type TestMultipleListingPrice struct {
-	listing                      Listing
+	listingStruct                listing.Listing
 	expectedPriceAfterCommission float64
 }
 
 func TestTDT(t *testing.T) {
 	listingCases := []TestMultipleListingPrice{
 		{
-			listing: Listing{
+			listingStruct: listing.Listing{
 				Id:    "1",
 				Name:  "11 Jackson Square",
 				Price: 1000000.00,
@@ -53,7 +57,7 @@ func TestTDT(t *testing.T) {
 			expectedPriceAfterCommission: 970000.00,
 		},
 		{
-			listing: Listing{
+			listingStruct: listing.Listing{
 				Id:    "2",
 				Name:  "255 Toa Payoh",
 				Price: 550000.00,
@@ -61,7 +65,7 @@ func TestTDT(t *testing.T) {
 			expectedPriceAfterCommission: 533500.00,
 		},
 		{
-			listing: Listing{
+			listingStruct: listing.Listing{
 				Id:    "3",
 				Name:  "12 Bukit Merah Road",
 				Price: 650400.00,
@@ -70,9 +74,9 @@ func TestTDT(t *testing.T) {
 		},
 	}
 
-	for _, listing := range listingCases {
-		if priceAfterCommission := CalculatePriceAfterCommission(listing.listing.Price); priceAfterCommission != listing.expectedPriceAfterCommission {
-			t.Errorf("listing price after commission %.2f expectedPriceAfterCommission %.2f listing price %.2f", priceAfterCommission, listing.expectedPriceAfterCommission, listing.listing.Price)
+	for _, l := range listingCases {
+		if priceAfterCommission := listing.CalculatePriceAfterCommission(l.listingStruct.Price); priceAfterCommission != l.expectedPriceAfterCommission {
+			t.Errorf("listing price after commission %.2f expectedPriceAfterCommission %.2f listing price %.2f", priceAfterCommission, l.expectedPriceAfterCommission, l.listingStruct.Price)
 		}
 	}
 }
